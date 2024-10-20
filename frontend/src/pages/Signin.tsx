7dc93bf2740a125fb9dd5ecb6bd1d72a5d650a14
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2024-10-20 11:20:37 +0530

    build errors were fixed but the full blof rendering was afoot, now that is fixed

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,15 +1,22 @@
 import Auth from "../components/Auth";
 import Quote from "../components/Quote";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export default function Signin(){
-    return<>
-    <div className="grid grid-cols-1 lg:grid-cols-2">
+export default function Signin() {
+    const handleLoginError = () => {
+        toast.error("Invalid username or password. Please try again.");
+    };
 
-<div>
-    <Auth type="signin"/>
-
-</div>
-<div className="hidden lg:block"><Quote/></div> 
-    </div>
-    </>
+    return (
+        <>
+            <ToastContainer />
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div>
+                    <Auth type="signin" onError={handleLoginError} />
+                </div>
+                <div className="hidden lg:block"><Quote /></div>
+            </div>
+        </>
+    );
 }
