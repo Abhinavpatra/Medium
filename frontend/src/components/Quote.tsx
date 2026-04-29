098e668cd09be5d0ccb390500f7114c7c9b30ee9
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2026-04-29 22:38:04 +0530

    fixed ui of quote

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,24 +1,25 @@
-export default function Quote(){
-    return <>
-     <div className="bg-slate-200 h-screen flex justify-center flex-col "> 
+export default function Quote() {
+  return (
+    <>
+      <div className="bg-slate-100 h-screen flex justify-center flex-col ">
         <div className="flex justify-center">
-            <div className="max-w-lg">
-                <div className="  text-2xl font-bold ">
-                    "The customer support i received was exceptional. The team went above and beyond to address my concerns."
-                </div>
-            
-                <div className=" text-left mt-4 text-xl font-semibold">
-                        Julies Winfield
-                </div>
-
-                <div className=" text-left text-sm font-light text-slate-500">
-                        CEO | Acme corp
-                </div>
+          <div className="max-w-lg">
+            <div className=" text-2xl font-bold text-slate-700 dark:text-slate-100 ">
+              "This guy is def either on drugs or abnormally driven."
             </div>
+
+            <div className=" text-left mt-4 text-xl font-semibold text-slate-700 dark:text-slate-100 ">
+              - Unknown
+            </div>
+
+            <div className=" text-left text-sm font-light text-slate-500">
+              CTO | Some Company
+            </div>
+          </div>
         </div>
-    </div>
-    
+      </div>
     </>
+  );
 }
 
 /* 
