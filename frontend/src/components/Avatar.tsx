01901037604b5d20c3cc2622205bc2bb2e2a1708
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2026-04-29 22:10:50 +0530

    added dark mode support to avatar component

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -1,8 +1,8 @@
 
 
 export default function Avatar({ name, size = "small" }: { name: string, size?: "small" | "big" }) {
-    return <div className={`relative inline-flex items-center justify-center overflow-hidden bg-gray-600 rounded-full ${size === "small" ? "w-6 h-6" : "w-10 h-10"}`}>
-    <span className={`${size === "small" ? "text-xs" : "text-md"} font-extralight text-gray-600 dark:text-gray-300`}>
+    return <div className={`relative inline-flex items-center justify-center overflow-hidden bg-gray-400 dark:bg-gray-600 rounded-full ${size === "small" ? "w-6 h-6" : "w-10 h-10"}`}>
+    <span className={`${size === "small" ? "text-xs" : "text-md"} font-extralight text-gray-700 dark:text-gray-200`}>
         {name[0]}
     </span>
 </div>
